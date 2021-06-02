32251d6b68b41ef82614e4e0af7fc222d6fbd3a7
hiroyuki miyazaki <miyazaki@mcommit.com>
2021-06-02 22:48:25 +0900

    WIP add dump elf header.

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,6 @@ use std::fs::File;
 use std::io::Read;
 use std::mem;
 
-const SIZE_ELF64_HALF: usize    = mem::size_of::<Elf64Half>();
-const SIZE_ELF64_WORD: usize    = mem::size_of::<Elf64Word>();
-const SIZE_ELF64_SWORD: usize   = mem::size_of::<Elf64Sword>();
-const SIZE_ELF64_ADDR: usize    = mem::size_of::<Elf64Addr>();
-const SIZE_ELF64_OFF: usize     = mem::size_of::<Elf64Off>();
-
-const EI_NIDENT: usize = 16;
-const ELF64_OFFSET_E_TYPE: usize    = EI_NIDENT;
-const ELF64_OFFSET_E_MACHINE: usize = ELF64_OFFSET_E_TYPE + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_VERSION: usize = ELF64_OFFSET_E_MACHINE + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_ENTRY: usize   = ELF64_OFFSET_E_VERSION + SIZE_ELF64_WORD;
-const ELF64_OFFSET_E_PHOFF: usize   = ELF64_OFFSET_E_ENTRY + SIZE_ELF64_ADDR;
-const ELF64_OFFSET_E_SHOFF: usize   = ELF64_OFFSET_E_PHOFF + SIZE_ELF64_OFF;
-const ELF64_OFFSET_E_FLAGS: usize   = ELF64_OFFSET_E_SHOFF + SIZE_ELF64_WORD;
-const ELF64_OFFSET_E_EHSIZE: usize   = ELF64_OFFSET_E_FLAGS + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_PHENTSIZE: usize   = ELF64_OFFSET_E_EHSIZE + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_PHNUM: usize   = ELF64_OFFSET_E_PHENTSIZE + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_SHENTSIZE: usize   = ELF64_OFFSET_E_PHNUM + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_SHNUM: usize   = ELF64_OFFSET_E_SHENTSIZE + SIZE_ELF64_HALF;
-const ELF64_OFFSET_E_SHSTRNDX: usize   = ELF64_OFFSET_E_SHNUM + SIZE_ELF64_HALF;
-
 type Elf32Half     = u16;
 type Elf64Half     = u16;
 type Elf32Word     = u32;
@@ -34,6 +13,27 @@ type Elf32Addr     = u32;
 type Elf64Addr     = u64;
 type Elf32Off      = u32;
 type Elf64Off      = u64;
+
+const SIZE_ELF64_HALF: usize    = mem::size_of::<Elf64Half>();
+const SIZE_ELF64_WORD: usize    = mem::size_of::<Elf64Word>();
+const SIZE_ELF64_SWORD: usize   = mem::size_of::<Elf64Sword>();
+const SIZE_ELF64_ADDR: usize    = mem::size_of::<Elf64Addr>();
+const SIZE_ELF64_OFF: usize     = mem::size_of::<Elf64Off>();
+
+const EI_NIDENT: usize = 16;
+const ELF64_OFFSET_E_TYPE: usize        = EI_NIDENT;
+const ELF64_OFFSET_E_MACHINE: usize     = ELF64_OFFSET_E_TYPE + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_VERSION: usize     = ELF64_OFFSET_E_MACHINE + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_ENTRY: usize       = ELF64_OFFSET_E_VERSION + SIZE_ELF64_WORD;
+const ELF64_OFFSET_E_PHOFF: usize       = ELF64_OFFSET_E_ENTRY + SIZE_ELF64_ADDR;
+const ELF64_OFFSET_E_SHOFF: usize       = ELF64_OFFSET_E_PHOFF + SIZE_ELF64_OFF;
+const ELF64_OFFSET_E_FLAGS: usize       = ELF64_OFFSET_E_SHOFF + SIZE_ELF64_WORD;
+const ELF64_OFFSET_E_EHSIZE: usize      = ELF64_OFFSET_E_FLAGS + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_PHENTSIZE: usize   = ELF64_OFFSET_E_EHSIZE + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_PHNUM: usize       = ELF64_OFFSET_E_PHENTSIZE + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_SHENTSIZE: usize   = ELF64_OFFSET_E_PHNUM + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_SHNUM: usize       = ELF64_OFFSET_E_SHENTSIZE + SIZE_ELF64_HALF;
+const ELF64_OFFSET_E_SHSTRNDX: usize    = ELF64_OFFSET_E_SHNUM + SIZE_ELF64_HALF;
 
 struct Elf64Ehdr
 {
@@ -71,7 +71,7 @@ impl Elf64Ehdr {
         buf_word.copy_from_slice(&bytes[ELF64_OFFSET_E_VERSION..ELF64_OFFSET_E_VERSION + SIZE_ELF64_WORD]);
         let e_version = Elf64Word::from_be_bytes(buf_word);
 
-        buf_dword.copy_from_slice(&bytes[ELF64_OFFSET_E_VERSION..ELF64_OFFSET_E_ENTRY + SIZE_ELF64_ADDR]);
+        buf_dword.copy_from_slice(&bytes[ELF64_OFFSET_E_ENTRY..ELF64_OFFSET_E_ENTRY + SIZE_ELF64_ADDR]);
         let e_entry = Elf64Addr::from_be_bytes(buf_dword);
 
         buf_dword.copy_from_slice(&bytes[ELF64_OFFSET_E_PHOFF..ELF64_OFFSET_E_PHOFF + SIZE_ELF64_OFF]);
@@ -127,8 +127,8 @@ fn main() {
     let mut buf = Vec::new();
     let _ret = f.read_to_end(&mut buf);
 
-    let mut elf64ehdr :Elf64Ehdr = Elf64Ehdr::new(&buf);
-    println!("{:?}", elf64ehdr.e_ident);
+    let elf64ehdr :Elf64Ehdr = Elf64Ehdr::new(&buf);
+    show_elf_header_info(elf64ehdr);
 
     /*
     unsafe {
@@ -141,4 +141,13 @@ fn main() {
     let contents = fs::read_to_string(&argv[1]).expect(&msg);
     println!("{}", contents);
     */
+}
+
+fn show_elf_header_info(elf64ehdr: Elf64Ehdr) {
+    println!("ELF Header:");
+    print!("  Magic:   ");
+    for by in elf64ehdr.e_ident.iter() {
+        print!("{:02x} ", by);
+    }
+    println!("");
 }
