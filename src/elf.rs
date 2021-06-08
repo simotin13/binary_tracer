29b5648b2ac4ffb9953771973567ed388ba9f7fe
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2021-06-08 17:16:38 +0900

    Support ELF32

--- src/elf.rs
+++ src/elf.rs
@@ -1,15 +1,22 @@
 use std::mem;
 
 type Elf32Half     = u16;
-type Elf64Half     = u16;
 type Elf32Word     = u32;
-type Elf64Word     = u32;
 type Elf32Sword    = i32;
-type Elf64Sword    = i32;
 type Elf32Addr     = u32;
-type Elf64Addr     = u64;
 type Elf32Off      = u32;
+
+type Elf64Half     = u16;
+type Elf64Word     = u32;
+type Elf64Sword    = i32;
+type Elf64Addr     = u64;
 type Elf64Off      = u64;
+
+const SIZE_ELF32_HALF: usize    = mem::size_of::<Elf32Half>();
+const SIZE_ELF32_WORD: usize    = mem::size_of::<Elf32Word>();
+const SIZE_ELF32_SWORD: usize   = mem::size_of::<Elf32Sword>();
+const SIZE_ELF32_ADDR: usize    = mem::size_of::<Elf32Addr>();
+const SIZE_ELF32_OFF: usize     = mem::size_of::<Elf32Off>();
 
 const SIZE_ELF64_HALF: usize    = mem::size_of::<Elf64Half>();
 const SIZE_ELF64_WORD: usize    = mem::size_of::<Elf64Word>();
@@ -57,6 +64,20 @@ const ET_DYN:u16    = 3;
 const ET_CORE:u16   = 4;
 const ET_NUM:u16    = 5;
 
+const ELF32_OFFSET_E_TYPE: usize        = EI_NIDENT;
+const ELF32_OFFSET_E_MACHINE: usize     = ELF32_OFFSET_E_TYPE + SIZE_ELF32_HALF;
+const ELF32_OFFSET_E_VERSION: usize     = ELF32_OFFSET_E_MACHINE + SIZE_ELF32_HALF;
+const ELF32_OFFSET_E_ENTRY: usize       = ELF32_OFFSET_E_VERSION + SIZE_ELF32_WORD;
+const ELF32_OFFSET_E_PHOFF: usize       = ELF32_OFFSET_E_ENTRY + SIZE_ELF32_ADDR;
+const ELF32_OFFSET_E_SHOFF: usize       = ELF32_OFFSET_E_PHOFF + SIZE_ELF32_OFF;
+const ELF32_OFFSET_E_FLAGS: usize       = ELF32_OFFSET_E_SHOFF + SIZE_ELF32_OFF;
+const ELF32_OFFSET_E_EHSIZE: usize      = ELF32_OFFSET_E_FLAGS + SIZE_ELF32_WORD;
+const ELF32_OFFSET_E_PHENTSIZE: usize   = ELF32_OFFSET_E_EHSIZE + SIZE_ELF32_HALF;
+const ELF32_OFFSET_E_PHNUM: usize       = ELF32_OFFSET_E_PHENTSIZE + SIZE_ELF32_HALF;
+const ELF32_OFFSET_E_SHENTSIZE: usize   = ELF32_OFFSET_E_PHNUM + SIZE_ELF32_HALF;
+const ELF32_OFFSET_E_SHNUM: usize       = ELF32_OFFSET_E_SHENTSIZE + SIZE_ELF32_HALF;
+const ELF32_OFFSET_E_SHSTRNDX: usize    = ELF32_OFFSET_E_SHNUM + SIZE_ELF32_HALF;
+
 const ELF64_OFFSET_E_TYPE: usize        = EI_NIDENT;
 const ELF64_OFFSET_E_MACHINE: usize     = ELF64_OFFSET_E_TYPE + SIZE_ELF64_HALF;
 const ELF64_OFFSET_E_VERSION: usize     = ELF64_OFFSET_E_MACHINE + SIZE_ELF64_HALF;
@@ -82,6 +103,24 @@ const ELF_MACHINES: [(u16, &str); 7] = [
 ];
 
 
+pub struct Elf32Ehdr
+{
+    e_ident:        [u8; EI_NIDENT],    // Magic number and other info
+    e_type:         Elf32Half,          // Object file type
+    e_machine:      Elf32Half,          // Architecture
+    e_version:      Elf32Word,          // Object file version
+    e_entry:        Elf32Addr,          // Entry point virtual address
+    e_phoff:        Elf32Off,           // Program header table file offset
+    e_shoff:        Elf32Off,           // Section header table file offset
+    e_flags:        Elf32Word,          // Processor-specific flags
+    e_ehsize:       Elf32Half,          // ELF header size in bytes
+    e_phentsize:    Elf32Half,          // Program header table entry size
+    e_phnum:        Elf32Half,          // Program header table entry count
+    e_shentsize:    Elf32Half,          // Section header table entry size
+    e_shnum:        Elf32Half,          // Section header table entry count
+    e_shstrndx:     Elf32Half,          // Section header string table index
+}
+
 pub struct Elf64Ehdr
 {
     e_ident:        [u8; EI_NIDENT],    // Magic number and other info
@@ -98,6 +137,136 @@ pub struct Elf64Ehdr
     e_shentsize:    Elf64Half,          // Section header table entry size
     e_shnum:        Elf64Half,          // Section header table entry count
     e_shstrndx:     Elf64Half,          // Section header string table index
+}
+
+impl Elf32Ehdr {
+    pub fn new(bytes: &[u8]) -> Elf32Ehdr {
+        let mut buf_half: [u8; 2] = Default::default();
+        let mut buf_word: [u8; 4] = Default::default();
+        let mut buf_dword: [u8; 8] = Default::default();
+
+        let mut e_ident :[u8; EI_NIDENT] = Default::default();
+        e_ident.copy_from_slice(&bytes[0..EI_NIDENT]);
+
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_TYPE..ELF32_OFFSET_E_TYPE + SIZE_ELF32_HALF]);
+        let e_type = Elf32Half::from_le_bytes(buf_half);
+
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_MACHINE..ELF32_OFFSET_E_MACHINE + SIZE_ELF32_HALF]);
+        let e_machine = Elf32Half::from_le_bytes(buf_half);
+
+        buf_word.copy_from_slice(&bytes[ELF32_OFFSET_E_VERSION..ELF32_OFFSET_E_VERSION + SIZE_ELF32_WORD]);
+        let e_version = Elf32Word::from_le_bytes(buf_word);
+
+        buf_word.copy_from_slice(&bytes[ELF32_OFFSET_E_ENTRY..ELF32_OFFSET_E_ENTRY + SIZE_ELF32_ADDR]);
+        let e_entry = Elf32Addr::from_le_bytes(buf_word);
+
+        buf_word.copy_from_slice(&bytes[ELF32_OFFSET_E_PHOFF..ELF32_OFFSET_E_PHOFF + SIZE_ELF32_OFF]);
+        let e_phoff =  Elf32Off::from_le_bytes(buf_word);
+
+        buf_word.copy_from_slice(&bytes[ELF32_OFFSET_E_SHOFF..ELF32_OFFSET_E_SHOFF + SIZE_ELF32_OFF]);
+        let e_shoff =  Elf32Off::from_le_bytes(buf_word);
+
+        buf_word.copy_from_slice(&bytes[ELF32_OFFSET_E_FLAGS..ELF32_OFFSET_E_FLAGS + SIZE_ELF32_WORD]);
+        let e_flags = Elf32Word::from_le_bytes(buf_word);
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_EHSIZE..ELF32_OFFSET_E_EHSIZE + SIZE_ELF32_HALF]);
+        let e_ehsize = Elf32Half::from_le_bytes(buf_half);
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_PHENTSIZE..ELF32_OFFSET_E_PHENTSIZE + SIZE_ELF32_HALF]);
+        let e_phentsize = Elf32Half::from_le_bytes(buf_half);
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_PHNUM..ELF32_OFFSET_E_PHNUM + SIZE_ELF32_HALF]);
+        let e_phnum = Elf32Half::from_le_bytes(buf_half);
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_SHENTSIZE..ELF32_OFFSET_E_SHENTSIZE + SIZE_ELF32_HALF]);
+        let e_shentsize = Elf32Half::from_le_bytes(buf_half);
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_SHNUM..ELF32_OFFSET_E_SHNUM + SIZE_ELF32_HALF]);
+        let e_shnum = Elf32Half::from_le_bytes(buf_half);
+        buf_half.copy_from_slice(&bytes[ELF32_OFFSET_E_SHSTRNDX..ELF32_OFFSET_E_SHSTRNDX + SIZE_ELF32_HALF]);
+        let e_shstrndx = Elf32Half::from_le_bytes(buf_half);
+        Elf32Ehdr {
+            e_ident: e_ident,
+            e_type: e_type,
+            e_machine: e_machine,
+            e_version: e_version,
+            e_entry: e_entry,
+            e_phoff: e_phoff,
+            e_shoff: e_shoff,
+            e_flags: e_flags,
+            e_ehsize: e_ehsize,
+            e_phentsize: e_phentsize,
+            e_phnum: e_phnum,
+            e_shentsize: e_shentsize,
+            e_shnum: e_shnum,
+            e_shstrndx: e_shstrndx,
+        }
+    }
+
+    pub fn show_elf_header_info(&self) {
+        println!("ELF Header:");
+        print!("  Magic:{:3}", "");
+        for by in self.e_ident.iter() {
+            print!("{:02x} ", by);
+        }
+        println!("");
+    
+        match self.e_ident[EI_CLASS] {
+            ELFCLASSNONE => println!("  Class:{:29}ELF None", ""),
+            ELFCLASS32 => println!("  Class:{:29}ELF32", ""),
+            ELFCLASS64 => println!("  Class:{:29}ELF64", ""),
+            _ => println!("  Class: {:29}{}", "", self.e_ident[EI_CLASS])
+        }
+    
+        match self.e_ident[EI_DATA] {
+            ELFDATANONE => println!("  Data:{:30}Invalid data encoding", ""),
+            ELFDATA2LSB => println!("  Data:{:30}2's complement, little endian", ""),
+            ELFDATA2MSB => println!("  Data:{:30}2's complement, big endian", ""),
+            _ => println!("  Data: {:30}{}", "", self.e_ident[EI_CLASS])
+        }
+    
+        println!("  Version:{:27}{} (current)", "", self.e_ident[EI_VERSION]);
+    
+        match self.e_ident[EI_OSABI] {
+            ELFOSABI_NONE => println!("  OS/ABI:{:28}UNIX - System V", ""),
+            ELFOSABI_SYSV => println!("  OS/ABI:{:28}Alias.", ""),
+            ELFOSABI_HPUX => println!("  OS/ABI:{:28}HP-UX", ""),
+            ELFOSABI_NETBSD => println!("  OS/ABI:{:28}NetBSD", ""),
+            ELFOSABI_GNU => println!("  OS/ABI:{:28}Object uses GNU ELF extensions.", ""),
+            ELFOSABI_LINUX => println!("  OS/ABI:{:28}Compatibility alias.", ""),
+            ELFOSABI_SOLARIS => println!("  OS/ABI:{:28}Sun Solaris.", ""),
+            ELFOSABI_AIX => println!("  OS/ABI:{:28}IBM AIX.", ""),
+            ELFOSABI_IRIX => println!("  OS/ABI:{:28}SGI Irix.", ""),
+            ELFOSABI_FREEBSD => println!("  OS/ABI:{:28}FreeBSD.", ""),
+            ELFOSABI_TRU64 => println!("  OS/ABI:{:28}Compaq TRU64 UNIX.", ""),
+            ELFOSABI_MODESTO => println!("  OS/ABI:{:28}Novell Modesto.", ""),
+            ELFOSABI_OPENBSD =>	println!("  OS/ABI:{:28}OpenBSD.", ""),
+            ELFOSABI_ARM_AEABI => println!("  OS/ABI:{:28}ARM EABI", ""),
+            ELFOSABI_ARM => println!("  OS/ABI:{:28}ARM", ""),
+            ELFOSABI_STANDALONE => println!("  OS/ABI:{:28}Standalone (embedded) application", ""),
+            _ => println!("  OS/ABI:{:28}{}", "", self.e_ident[EI_OSABI])
+        }
+    
+        println!("  ABI Version:{:23}{}", "", self.e_ident[EI_ABIVERSION]);
+    
+        print!("  Type:{:30}", "");
+        match self.e_type {
+            ET_NONE => println!("None"),
+            ET_REL	=> println!("REL (Relocatable file)"),
+            ET_EXEC	=> println!("EXE (Executable file)"),
+            ET_DYN  => println!("DYN (Shared object file)"),
+            ET_CORE => println!("Core"),
+            _ => println!("{}", self.e_type)
+        }
+    
+        println!("  Machine:{:27}{}", "", get_machine_name(self.e_machine));
+        println!("  Version:{:27}0x{:x}", "", self.e_version);
+        println!("  Entry point address:{:15}0x{:x}", "", self.e_entry);
+        println!("  Start of program headers:{:10}{} (bytes into file)", "", self.e_phoff);
+        println!("  Start of section headers:{:10}{} (bytes into file)", "", self.e_shoff);
+        println!("  Flags:{:29}0x{:x}", "", self.e_flags);
+        println!("  Size of this header:{:15}{} (bytes)", "", self.e_ehsize);
+        println!("  Size of program headers:{:11}{} (bytes)", "", self.e_phentsize);
+        println!("  Number of program headers:{:9}{}", "", self.e_phnum);
+        println!("  Size of section headers:{:11}{} (bytes)", "", self.e_shentsize);
+        println!("  Number of section headers:{:9}{}", "", self.e_shnum);
+        println!("  Section header string table index:{:1}{}", "", self.e_shstrndx);
+    }
 }
 
 impl Elf64Ehdr {
@@ -208,14 +377,14 @@ impl Elf64Ehdr {
         print!("  Type:{:30}", "");
         match self.e_type {
             ET_NONE => println!("None"),
-            ET_REL	=> println!("REL"),
+            ET_REL	=> println!("REL (Relocatable file)"),
             ET_EXEC	=> println!("EXE (Executable file)"),
             ET_DYN  => println!("DYN (Shared object file)"),
             ET_CORE => println!("Core"),
             _ => println!("{}", self.e_type)
         }
     
-        println!("  Machine:{:27}{}", "", self.get_machine_name());
+        println!("  Machine:{:27}{}", "", get_machine_name(self.e_machine));
         println!("  Version:{:27}0x{:x}", "", self.e_version);
         println!("  Entry point address:{:15}0x{:x}", "", self.e_entry);
         println!("  Start of program headers:{:10}{} (bytes into file)", "", self.e_phoff);
@@ -228,13 +397,36 @@ impl Elf64Ehdr {
         println!("  Number of section headers:{:9}{}", "", self.e_shnum);
         println!("  Section header string table index:{:1}{}", "", self.e_shstrndx);
     }
+}
 
-    fn get_machine_name(&self) -> String {
-        for machine in &ELF_MACHINES {
-            if self.e_machine == machine.0 {
-                return machine.1.to_string();
-            }
-        }
-        return format!("Unknown Machine:[{}]", self.e_machine);
+pub fn is_elf(bytes: &[u8]) -> bool {
+    if bytes[0] != 0x7F {
+        return false;
     }
+    if bytes[1] != 0x45 {
+        return false;
+    }
+    if bytes[2] != 0x4C {
+        return false;
+    }
+    if bytes[3] != 0x46 {
+        return false;
+    }
+    true
+}
+pub fn is_elf32(bytes: &[u8]) -> bool {
+    bytes[EI_CLASS] == ELFCLASS32
+}
+
+pub fn is_elf64(bytes: &[u8]) -> bool {
+    bytes[EI_CLASS] == ELFCLASS64
+}
+
+pub fn get_machine_name(e_machine:u16) -> String {
+    for machine in &ELF_MACHINES {
+        if e_machine == machine.0 {
+            return machine.1.to_string();
+        }
+    }
+    return format!("Unknown Machine:[{}]", e_machine);
 }
--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,21 @@ fn main() {
     let mut f = File::open(&argv[1]).expect(&msg);
     let mut buf = Vec::new();
     let _ret = f.read_to_end(&mut buf);
+    if elf::is_elf(&buf) != true {
+        eprintln!("{} is not ELF Formart File", argv[1]);
+        std::process::exit(-1);
+    }
 
-    let elf64ehdr: elf::Elf64Ehdr = elf::Elf64Ehdr::new(&buf);
-    elf64ehdr.show_elf_header_info();
+    if elf::is_elf32(&buf) {
+        let elf32ehdr: elf::Elf32Ehdr = elf::Elf32Ehdr::new(&buf);
+        elf32ehdr.show_elf_header_info();
+    }
+
+    if elf::is_elf64(&buf) {
+        let elf64ehdr: elf::Elf64Ehdr = elf::Elf64Ehdr::new(&buf);
+        elf64ehdr.show_elf_header_info();
+    }
+
 
     // start gdb
     let mut gdbmi = gdbmi::GdbMi::new(&argv[1], 1000);
